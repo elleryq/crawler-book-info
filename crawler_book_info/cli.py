e6c0e8cceeb959481bc43ea65350bdb5c2fd05a5
Yan-ren Tsai <ellery@solidyear.com.tw>
2019-08-08 14:18:26 +0800

    Implement main()
    Make it can crawl according to the host

--- crawler_book_info/cli.py
+++ crawler_book_info/cli.py
@@ -3,10 +3,28 @@ crawler-book-info
 ==========
 License: MIT, see LICENSE for more details.
 """
+import sys
+from urllib.parse import urlparse
+from crawler_book_info import tenlong, books
+
 
 def main():
-  pass
+    if len(sys.argv) < 2:
+        print("Need url.")
+        sys.exit(-1)
+
+    netloc_mapping = (
+        ('tenlong.com.tw', tenlong.crawl_tenlong, tenlong.to_html),
+        ('books.com.tw', books.crawl_books_com_tw, books.to_html),
+    )
+    url = sys.argv[1]
+    o = urlparse(url)
+    for netloc, crawl, to_html in netloc_mapping:
+      if netloc in o.netloc:
+        data = crawl(url)
+        to_html(data)
+        break
 
 
 if __name__ == "__main__":
-  main()
+    main()
